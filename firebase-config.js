e5e5ceee9f70695b43915065316abf1a2bc35fb9
Bhawna2567 <bhawna.sharma@moe.sch.ae>
2026-06-21 21:56:48 +0400

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -7,3 +7,7 @@ var firebaseConfig = {
   messagingSenderId: "476980914736",
   appId: "1:476980914736:web:43051aacbcb02c09ea1bf5"
 };
+
+// App Check (anti-bot). Paste your reCAPTCHA v3 site key from Firebase Console here,
+// then re-upload this file. Leave as-is to keep App Check off for now.
+var APPCHECK_SITE_KEY = "6LdlFiwtAAAAAK6ctAPdpLuU7J0pCvMF0Au66zl-";
